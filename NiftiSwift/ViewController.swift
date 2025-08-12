f7f4dd447e40ab053bad380d95d775f4005e7007
Pete Molfese <pmolfese@gmail.com>
2025-08-12 12:09:26 -0400

    Initial Commit

---
+++ NiftiSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NiftiSwift
+//
+//  Created by Molfese, Peter  [E] on 8/12/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
